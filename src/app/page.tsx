ff32f90b131985d57f1451996b8191261cc96aa4
HubBusinessUP <antonybusinesshub@gmail.com>
2026-03-28 12:23:32 +0100

    homepage: author name Antonio -> Antony

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ export default async function HomePage() {
               title={article.title}
               excerpt={article.excerpt}
               categoryName={article.category?.name || ""}
-              author={article.author || "Antonio"}
+              author={article.author || "Antony"}
               readingTime={article.reading_time}
               coverImage={article.cover_image}
               coverAlt={article.cover_alt || article.title}
