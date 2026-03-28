b2d7d8c4ed82f685280049cc8c0597201db483b5
HubBusinessUP <antonybusinesshub@gmail.com>
2026-03-28 09:52:37 +0100

    update Sidebar.tsx with categories counter

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,23 @@
+import Link from "next/link";
+import { getCategoriesWithCount } from "@/lib/queries";
+
 const TG_LINK = "https://t.me/+oMvBwUP3_xNmNDM8";
 
-export default function Sidebar() {
+const DOT_COLORS = [
+  "var(--c4)",
+  "var(--c3)",
+  "var(--c5)",
+  "var(--c2)",
+  "var(--c6)",
+  "var(--c1)",
+];
+
+export default async function Sidebar() {
+  const categoriesWithCount = await getCategoriesWithCount();
+
   return (
     <aside className="sidebar">
+      {/* Ad Banner */}
       <div className="sb-block sb-ad">
         <div className="sb-ad-inner">
           <div className="sb-ad-label">// Spazio Pubblicitario</div>
@@ -12,6 +27,34 @@ export default function Sidebar() {
         </div>
       </div>
 
+      {/* Categorie con contatore */}
+      {categoriesWithCount.length > 0 && (
+        <div className="sb-block">
+          <div className="sb-head">
+            <span className="slash">//</span> Categorie
+          </div>
+          <div className="sb-cats">
+            {categoriesWithCount.map((item, i) => (
+              <Link
+                key={item.category.id}
+                href={`/categoria/${item.category.slug}`}
+                className="sb-cat-item"
+              >
+                <div className="sb-cat-name">
+                  <div
+                    className="sb-cat-dot"
+                    style={{ background: DOT_COLORS[i % DOT_COLORS.length] }}
+                  />
+                  {item.category.name}
+                </div>
+                <span className="sb-cat-count">{item.count}</span>
+              </Link>
+            ))}
+          </div>
+        </div>
+      )}
+
+      {/* Telegram */}
       <div className="sb-block">
         <div className="tg-box">
           <div className="tg-box-title">Hub Telegram</div>
